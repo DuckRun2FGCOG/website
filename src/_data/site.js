9a1878a2f2d4e75e40305aa40f3bc5daecfc7d8d
Carlos Freund <carlosfreund@gmail.com>
2026-05-15 17:09:48 -0600

    ADDED pastors.

--- src/_data/site.js
+++ src/_data/site.js
@@ -6,5 +6,5 @@ module.exports = {
   },
   repo: "duckrun2fgcog/website",
   mapsEmbedUrl:
-    "https://maps.google.com/maps?q=Duck+Run+2%2C+Cayo+District%2C+Belize&t=&z=14&ie=UTF8&iwloc=&output=embed",
+    "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d654.4238524375068!2d-89.02510599505419!3d17.245152529922176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbz!4v1778883491639!5m2!1sen!2sbz",
 };
